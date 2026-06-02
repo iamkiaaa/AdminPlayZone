3efd002c87cdee2b42630fb145ef1d8518bbb1bc
unknown <adzghan@gmail.com>
2026-06-02 17:37:35 +0700

    load playzone js before page scripts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -111,8 +111,7 @@
       </div>
     </div>
   </div>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.0/chart.umd.min.js"></script>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+  <script src="/js/playzone.js"></script>
   @stack('scripts')
 </body>
 
